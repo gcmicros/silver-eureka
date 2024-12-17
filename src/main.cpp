2dcfa26c49e5e0ba3df7ab57b53bf3f81f74e5ce
George C <george.micros@gmail.com>
2024-12-17 09:55:01 -0500

    Clean up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -14,10 +13,8 @@
 using namespace std::chrono;
 
 int main() {
-    auto start = high_resolution_clock::now();
     std::ifstream input("elephant.ts", std::ios::binary);
     std::vector<unsigned char> data(std::istreambuf_iterator<char>(input), {});
-    std::cout << "read: " << duration_cast<milliseconds>(high_resolution_clock::now() - start).count() << std::endl;
     
     bool foundPAT = false;
     bool foundPMT = false;
@@ -30,17 +27,13 @@ int main() {
     std::map<unsigned int, bool> found_pmts;
     std::vector<unsigned char>::iterator it = data.begin();
 
-    int cnt = 0;
     // seach for the sync byte
     while ((it = std::find(it, data.end(), 'G')) != data.end()) {
-        cnt++;
         // parse the packet and get the PID
         auto start = const_cast<unsigned char*const>(&(*it));
         TSPacket packet(start);
         const auto PID = packet.getPID();
         
-        std::cout << std::dec << "count: " << cnt << 
-            " -> " << PID << " " << packet.getPUSI() << std::endl;
         // only look for PAT once
         if (PID == 0x00 && !foundPAT) {
             ProgramAssociationTable PAT(packet.getPUSI());
@@ -56,13 +49,12 @@ int main() {
         const auto foundPMT = found_pmts.find(PID);
         if (foundPMT != found_pmts.end() && !(foundPMT->second)) {
             // parse this specific PMT
-            std::cout << "parse PMT" << std::endl;
-            std::cout << std::dec << "count: " << cnt << std::endl;
             found_pmts[PID] = true;
             ProgramMappingTable PMT(packet.getPUSI());
             PMT.parse(packet.getPayloadStart());
             const auto es = PMT.getElemStreamPIDS();
 
+            // TODO(gmicros): fix magic numbers
             video_pid = es.find((unsigned int)0x1b)->second;
             foundVideo = true;
             audio_pid = es.find((unsigned int)0x0f)->second;
@@ -71,40 +63,25 @@ int main() {
             std::cout << "video: " << video_pid << std::endl
                     << "audio: " << audio_pid << std::endl;
         }
-
-    
         
-        //std::cout << "offset: " << std::dec << (packet.getPayloadStart() - start) << std::endl;
-        //std::cout << "payload length: " << std::dec << packet.getPayloadSize() << std::endl;
         if (PID == video_pid && foundVideo) {
-            //std::cout << std::dec << "PUSI: " << packet.getPUSI() << std::endl;
-            //std::cout << std::dec << "count: " << cnt << std::endl;
             if (packet.getPUSI()) {
-                std::cout << "video pusi" << std::endl;
                 PES pes(packet.getPayloadStart());
-                const unsigned int diff = pes.getPacketStart() - packet.getPayloadStart();
-                std::cout << std::dec << "packet: " << pes.getPacketLength() << " " << diff <<  std::endl;
                 video_file.appendChunk(pes.getPacketStart(), packet.getPayloadSize() - pes.getPacketLength());
             } else {
                 video_file.appendChunk(packet.getPayloadStart(), packet.getPayloadSize());
             }
-            
-            //video_file.appendChunk(packet.getPayloadStart(), packet.getPayloadSize());
         }
 
         if (PID == audio_pid && foundAudio) {
-            //PES pes(packet.getPayloadStart());
             if (packet.getPUSI()) {
-                std::cout << "audio pusi" << std::endl;
                 PES pes(packet.getPayloadStart());
-                const unsigned int diff = pes.getPacketStart() - packet.getPayloadStart();
-                std::cout << std::dec << diff << std::endl;
                 audio_file.appendChunk(pes.getPacketStart(), packet.getPayloadSize() - pes.getPacketLength());
             } else {
                 audio_file.appendChunk(packet.getPayloadStart(), packet.getPayloadSize());
             }
         }
         // jump to end of this packet
-        it += 184;
+        it += 188;
     }
 }
